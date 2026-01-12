4deba60f1c063ad5f6d85d7bbd1fa94b66987e3f
Astro-Syn <kelseyamyb@gmail.com>
2026-01-12 12:07:29 -0500

    added in blueprint bg, positioned character more accurately

--- index.js
+++ index.js
@@ -12,16 +12,40 @@ window.onload = function() {
     canvasElement.width = GameViewport.WIDTH;
     canvasElement.height = GameViewport.HEIGHT;
 
-    const spork = document.querySelector('img');
+    const [spork, bg] = document.querySelectorAll('img');
+    
 
-    ctx.strokeStyle = 'black';
-    ctx.moveTo(0, 0);
-    ctx.lineTo(GameViewport.WIDTH, GameViewport.HEIGHT);
-    ctx.moveTo(GameViewport.WIDTH, 0);
-    ctx.lineTo(0, GameViewport.HEIGHT);
-    ctx.stroke();
+    const position = {
+        x: GameViewport.WIDTH / 2 - spork.width / 2,
+        y: 150,
+    }
 
-    ctx.drawImage(spork, 0, 0);
+    let velocity = 1;
 
-    console.log(ctx);
+    function frame() {
+    position.x += velocity;
+
+    if(position.x > GameViewport.WIDTH -spork.width || position.x < 0){
+        velocity = -velocity;
+    }
+
+    //ctx.clearRect(0, 0, GameViewport.WIDTH, GameViewport.HEIGHT);
+    ctx.drawImage(bg, 0, 0);
+
+    //ctx.strokeStyle = 'black';
+    //ctx.moveTo(0, 0);
+    //ctx.lineTo(GameViewport.WIDTH, GameViewport.HEIGHT);
+    //ctx.moveTo(GameViewport.WIDTH, 0);
+    //ctx.lineTo(0, GameViewport.HEIGHT);
+    //ctx.stroke();
+
+    ctx.drawImage(spork, position.x, position.y); 
+    
+
+    window.requestAnimationFrame(frame);
+    }
+
+    window.requestAnimationFrame(frame);
+
+   
 }
