807cc19c9aa5da5db3469ac83452215f673e8345
Astro-Syn <kelseyamyb@gmail.com>
2026-01-30 13:01:46 -0500

    updated sprites

--- fighters/Vexel.js
+++ fighters/Vexel.js
@@ -59,6 +59,9 @@ import { CharacterState, PushBox } from "../src/constants/character.js";
         ['jumpBackwards-6', [[[351, 259, 55, spriteHeight], [21, 70]], PushBox.JUMP]],
 
         ['crouch-1', [[[226, 372, spriteWidth, spriteHeight], [20, 55]], PushBox.CROUCH]],
+
+        ['lightMeele', [[[89, 453, 55, spriteHeight], [21, 70]], PushBox.IDLE]],
+        ['lightMeele', [[[157, 453, spriteWidth, spriteHeight], [21, 70]], PushBox.IDLE]],
         ]);
 
           this.animations = {
